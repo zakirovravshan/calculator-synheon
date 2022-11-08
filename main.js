ca3191a259dc0ad23adb7a4d4f6d8cfcfee99551
zakirovravshan <zakirovrovshan@gmail.com>
2022-11-08 22:02:37 +0500

    edit some text

--- main.js
+++ main.js
@@ -8,19 +8,19 @@ elForm.addEventListener("submit" , function(evt) {
     evt.preventDefault()
     console.log("submit boldi");
     if(elSel.value == 35){
-        elWater.textContent = elInp.value * 0.5 + " - литров"
-        elFreeze.textContent = elInp.value * 0.5  + " - литров"
+        elWater.textContent = elInp.value * 0.5 + " - литр"
+        elFreeze.textContent = elInp.value * 0.5  + " - литр"
     }
     if(elSel.value == 25){
-        elWater.textContent = (elInp.value * 0.6).toFixed(1) + " - литров"
-        elFreeze.textContent = (elInp.value * 0.4).toFixed(1) + " - литров"
+        elWater.textContent = (elInp.value * 0.6).toFixed(1) + " - литр"
+        elFreeze.textContent = (elInp.value * 0.4).toFixed(1) + " - литр"
     }
     if(elSel.value == 15){
-        elWater.textContent = (elInp.value * 0.7).toFixed(1) + " - литров"
-        elFreeze.textContent = (elInp.value * 0.3).toFixed(1) + " - литров"
+        elWater.textContent = (elInp.value * 0.7).toFixed(1) + " - литр"
+        elFreeze.textContent = (elInp.value * 0.3).toFixed(1) + " - литр"
     }
     if(elSel.value == 8){
-        elWater.textContent = (elInp.value * 0.8).toFixed(1) + " - литров"
-        elFreeze.textContent = (elInp.value * 0.2).toFixed(1) + " - литров"
+        elWater.textContent = (elInp.value * 0.8).toFixed(1) + " - литр"
+        elFreeze.textContent = (elInp.value * 0.2).toFixed(1) + " - литр"
     }
 })
