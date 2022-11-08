9bc0e91dd74d3448e3c5c8472279f9580809a999
zakirovravshan <zakirovrovshan@gmail.com>
2022-11-08 21:53:47 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+let elInp = document.querySelector(".calculator__inp")
+let elSel = document.querySelector(".calculator__select")
+let elForm = document.querySelector(".calculator__form")
+let elWater = document.querySelector(".water")
+let elFreeze = document.querySelector(".antifreeze")
+
+elForm.addEventListener("submit" , function(evt) {
+    evt.preventDefault()
+    console.log("submit boldi");
+    if(elSel.value == 35){
+        elWater.textContent = elInp.value * 0.5 + " - литров"
+        elFreeze.textContent = elInp.value * 0.5  + " - литров"
+    }
+    if(elSel.value == 25){
+        elWater.textContent = (elInp.value * 0.6).toFixed(1) + " - литров"
+        elFreeze.textContent = (elInp.value * 0.4).toFixed(1) + " - литров"
+    }
+    if(elSel.value == 15){
+        elWater.textContent = (elInp.value * 0.7).toFixed(1) + " - литров"
+        elFreeze.textContent = (elInp.value * 0.3).toFixed(1) + " - литров"
+    }
+    if(elSel.value == 8){
+        elWater.textContent = (elInp.value * 0.8).toFixed(1) + " - литров"
+        elFreeze.textContent = (elInp.value * 0.2).toFixed(1) + " - литров"
+    }
+})
